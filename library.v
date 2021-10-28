0d462a35afaf60a735e76a9aaca7044aefe349f5
Tharun S J <tharunkumar200230@gmail.com>
2021-10-28 23:34:01 +0530

    Add files via upload

---
+++ library.v
@@ -0,0 +1,69 @@
+module invert(output ib,input b);
+	assign ib = ~b;
+endmodule
+
+module and2 (input wire i0, i1, output wire o);
+  assign o = i0 & i1;
+endmodule
+
+module or2 (input wire i0, i1, output wire o);
+  assign o = i0 | i1;
+endmodule
+
+module xor2 (input wire i0, i1, output wire o);
+  assign o = i0 ^ i1;
+endmodule
+
+module nand2 (input wire i0, i1, output wire o);
+   wire t;
+   and2 and2_0 (i0, i1, t);
+   invert invert_0 (t, o);
+endmodule
+
+module nor2 (input wire i0, i1, output wire o);
+   wire t;
+   or2 or2_0 (i0, i1, t);
+   invert invert_0 (t, o);
+endmodule
+
+module xnor2 (input wire i0, i1, output wire o);
+   wire t;
+   xor2 xor2_0 (i0, i1, t);
+   invert invert_0 (t, o);
+endmodule
+
+module and3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   and2 and2_0 (i0, i1, t);
+   and2 and2_1 (i2, t, o);
+endmodule
+
+module or3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   or2 or2_0 (i0, i1, t);
+   or2 or2_1 (i2, t, o);
+endmodule
+
+module nor3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   or2 or2_0 (i0, i1, t);
+   nor2 nor2_0 (i2, t, o);
+endmodule
+
+module nand3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   and2 and2_0 (i0, i1, t);
+   nand2 nand2_1 (i2, t, o);
+endmodule
+
+module xor3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   xor2 xor2_0 (i0, i1, t);
+   xor2 xor2_1 (i2, t, o);
+endmodule
+
+module xnor3 (input wire i0, i1, i2, output wire o);
+   wire t;
+   xor2 xor2_0 (i0, i1, t);
+   xnor2 xnor2_0 (i2, t, o);
+endmodule
